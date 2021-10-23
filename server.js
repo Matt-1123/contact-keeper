c051ed7326551d417f72a33918d1b0ec902e7bd3
Matt Russo <matthew.russo91@gmail.com>
2021-10-23 11:42:54 -0400

    setup backend routes

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contact Keeper API" })
 );
 
+// Define Routes (every backend route will start with '/api')
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 // First looks for an environment variable called PORT, which will be used in production. Uses port 5000 in development
 const PORT = process.env.PORT || 5000;
 
