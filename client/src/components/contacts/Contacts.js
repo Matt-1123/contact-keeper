9f9a82c015c2a91602db597d6d426a7d64bbf815
Matt Russo <matthew.russo91@gmail.com>
2021-11-14 10:35:40 -0500

    add ContactItem animations with react-transition-group

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
 
@@ -14,17 +15,23 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered !== null && filtered.length === 0 ? (
-        <h4>No results found.</h4>
-      ) : filtered !== null ? (
-        filtered.map((contact) => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))
-      ) : (
-        contacts.map((contact) => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))
-      )}
+      <TransitionGroup>
+        {filtered !== null && filtered.length === 0 ? (
+          <h4>No results found.</h4>
+        ) : filtered !== null ? (
+          filtered.map((contact) => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))
+        ) : (
+          contacts.map((contact) => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))
+        )}
+      </TransitionGroup>
     </Fragment>
   );
 };
